495d36e00fd8db8d86bd8f2c0b22ecc4b6a7dbf7
Simja <yorkamacua@gmail.com>
2024-05-17 10:15:35 -0600

    chore: Update base URL for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://simjaMalkeinu.github.io/schedule-assistant-front/"
+  base: '/schedule-assistant-front/',
 })
