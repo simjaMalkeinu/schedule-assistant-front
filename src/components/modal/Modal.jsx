a50bcdf6212f7f12f1f6c98edee8d41a8c5ffa86
Simja <yorkamacua@gmail.com>
2024-05-22 19:13:10 -0600

    actualizacion

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,12 +1,23 @@
-export default function Modal() {
+import PropTypes from 'prop-types'
+import './modal.css'
+
+export default function Modal(props) {
+
+  const { update } = props;
+
   return (
     <div>
-        <div className="modal">
+        <div className="modal open">
             <div className="modal-content">
             <span className="close-button">&times;</span>
             <p>Some text in the Modal..</p>
+            <button onClick={update}>Close</button>
             </div>
         </div>
     </div>
   )
 }
+
+Modal.propTypes = {
+  update: PropTypes.func,
+};
