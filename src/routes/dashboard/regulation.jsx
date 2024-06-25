a41cc9ba1c68c401351a152bb54421b0aeefeb91
Pablo Cruz <pcruzp1400@gmail.com>
2024-06-25 04:06:02 -0600

    chore: add regulation content

--- src/routes/dashboard/regulation.jsx
+++ src/routes/dashboard/regulation.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
 import SideBar from "../../components/sidebar/sidebar";
 import Situations from "../../components/situations/situations";
+import ContainerReglamento from "../../components/regulation/ContainerReglamento";
 import Container from "../../components/regulation/Container";
+import { data } from "../../utils/regulation";
 
 export default function Regulation() {
+  const [situation, setSituation] = useState("REGULAR");
+  console.log(situation);
+  const handleSituation = (situationSelected) => {
+    setSituation(situationSelected);
+  };
   return (
     <>
       <aside className="hidden lg:block w-[300px]">
@@ -13,14 +21,65 @@ export default function Regulation() {
           REGLAMENTO PARA INSCRIPCIÓN
         </h1>
         <section className="flex flex-wrap sm:flex-row gap-x-3 py-4">
-          <Situations situationType={"Regulary"} />
-          <Situations situationType={"No Regulary"} />
-          <Situations situationType={"Dictaminary"} />
+          <button onClick={() => handleSituation("REGULAR")}>
+            <Situations situationType={"REGULAR"} />
+          </button>
+          <button onClick={() => handleSituation("IRREGULAR")}>
+            <Situations situationType={"IRREGULAR"} />
+          </button>
+          <button onClick={() => handleSituation("DICTAMEN")}>
+            <Situations situationType={"DICTAMEN"} />
+          </button>
         </section>
-        <section className="flex flex-col gap-5">
-          <Container title="Reglamento del IPN." />
-          <Container title="Procedimientos." />
-          <Container title="Consideraciones." />
+        <section>
+          {situation === "REGULAR" && (
+            <div className="flex flex-col gap-7">
+              <ContainerReglamento
+                title="Reglamento del IPN."
+                content={data.REGULAR.reglamento}
+              />
+              <Container
+                title="Procedimientos."
+                content={data.REGULAR.procedimientos}
+              />
+              <Container
+                title="Consideraciones."
+                content={data.REGULAR.consideraciones}
+              />
+            </div>
+          )}
+          {situation === "IRREGULAR" && (
+            <div className="flex flex-col gap-7">
+              <ContainerReglamento
+                title="Reglamento del IPN."
+                content={data.IRREGULAR.reglamento}
+              />
+              <Container
+                title="Procedimientos."
+                content={data.IRREGULAR.procedimientos}
+              />
+              <Container
+                title="Consideraciones."
+                content={data.IRREGULAR.consideraciones}
+              />
+            </div>
+          )}
+          {situation === "DICTAMEN" && (
+            <div className="flex flex-col gap-7">
+              <ContainerReglamento
+                title="Reglamento del IPN."
+                content={data.DICTAMEN.reglamento}
+              />
+              <Container
+                title="Procedimientos."
+                content={data.DICTAMEN.procedimientos}
+              />
+              <Container
+                title="Consideraciones."
+                content={data.DICTAMEN.consideraciones}
+              />
+            </div>
+          )}
         </section>
       </main>
     </>
